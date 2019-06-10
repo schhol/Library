7411544a7807564d581e4146782b98de0ae44e60
nikin <nikin@DESKTOP-521FD5L>
2019-06-10 14:38:48 +0300

    First one dudes

---
+++ Library_System_JAVA/src/main/java/com/example/demo/LibrarySystemJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarySystemJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrarySystemJavaApplication.class, args);
+	}
+
+}
