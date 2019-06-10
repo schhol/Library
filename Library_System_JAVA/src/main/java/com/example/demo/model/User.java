971c7e1d6f770bb6c89ad184f3d8d917daf8d8b6
Admin <Admin@DESKTOP-T5ES982>
2019-06-10 15:02:46 +0300

    User changes

---
+++ Library_System_JAVA/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,67 @@
+package com.example.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "UserTable")
+public class User {
+
+		@Id
+		@GeneratedValue(strategy = GenerationType.AUTO)
+		@Column(name = "Id_u")
+		private int id_u;
+		
+		@NotNull
+		@Size (min = 3, max = 10)
+		@Column(name = "Username")
+		private String username;
+		
+		@NotNull
+		@Size (min = 3, max = 10)
+		@Column(name = "Password")
+		private String password;
+
+		
+		//Constructors
+		public User() {
+			super();
+		}
+
+		public User( @NotNull @Size(min = 3, max = 10) String username,
+				@NotNull @Size(min = 3, max = 10) String password) {
+			super();
+			this.username = username;
+			this.password = password;
+		
+		}
+
+		//Get && Set
+		public int getId_u() {
+			return id_u;
+		}
+
+
+		public String getUsername() {
+			return username;
+		}
+
+		public void setUsername(String username) {
+			this.username = username;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;		
+		}
+}
