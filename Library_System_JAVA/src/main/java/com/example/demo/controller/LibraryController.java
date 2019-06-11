ddcd6584adf87ae34a74d27852d7594fb5059de8
nikin <nikin@DESKTOP-521FD5L>
2019-06-11 15:50:26 +0300

    No testing data, registration works

--- Library_System_JAVA/src/main/java/com/example/demo/controller/LibraryController.java
+++ Library_System_JAVA/src/main/java/com/example/demo/controller/LibraryController.java
@@ -1,11 +1,5 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.Date;
-import java.util.GregorianCalendar;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,55 +34,6 @@ public class LibraryController {
 	@Autowired
 	LibraryDepartmentRepo libraryDepartmentRepo;
 	
-	//viss ir labi
-	
-	@GetMapping(value = "/testingData")
-	public String testingData(){
-
-		//Users for readers
-		User u1 = new User("Janis", "111");
-		User u2 = new User("Liga", "222");
-		User u3 = new User("Juris", "333");
-				
-		//Users for employees
-		User u4 = new User("Anna", "444");
-		User u5 = new User("Ivo", "555");
-		User u6 = new User("Baiba", "666");
-		
-		userRepo.save(u1);
-		userRepo.save(u2);
-		userRepo.save(u3);
-		userRepo.save(u4);
-		userRepo.save(u5);
-		userRepo.save(u6);
-		
-		LibraryDepartment ld1 = new LibraryDepartment("Jokes");
-		libraryDepartmentRepo.save(ld1);
-		
-		Employee emp1 = new Employee("Anna", "Berzina", ld1, u4);
-		Employee emp2 = new Employee("Ivo", "Berzins", ld1, u5);
-		employeeRepo.save(emp1);
-		employeeRepo.save(emp2);
-		
-		Reader r1 = new  Reader("Janis", "Kalnins", u1);
-		Reader r2 = new  Reader("Liga", "Kalnina", u2);
-		readerRepo.save(r1);
-		readerRepo.save(r2);
-		
-		
-		
-		Book b1 = new Book("1111111111", 2010 ,"book", "Dude", 4.9, "GOOD", "Common", "Good book about dudes", ld1);
-		Book b2 = new Book("1112211111", 2010 ,"gramata", "Picasso", 4.5, "GOOD", "Common", "Good book about art", ld1);
-
-		bookRepo.save(b1);
-		bookRepo.save(b2);
-				
-
-		
-		System.out.println("User count: " + userRepo.count());
-		
-		return "ok";
-	}
 	
 	//Home screen
 	//kim
@@ -137,15 +82,17 @@ public class LibraryController {
 		String username = user.getUsername();
 		String password = user.getPassword();
 		
-		User newUser = new User(username, password);
-		Reader newReader = new Reader(name, surname, newUser);
-		newUser.setReader(newReader);
-		
 		User userTemp = userRepo.findByUsernameAndPassword(user.getUsername(), user.getPassword());
 		
 		if(userTemp == null) {
+			
+			User newUser = new User(username, password);
 			userRepo.save(newUser);
+			Reader newReader = new Reader(name, surname, newUser);
 			readerRepo.save(newReader);
+			newUser.setReader(newReader);
+			userRepo.save(newUser);
+			
 			return "redirect:/authorise";
 		}
 		
