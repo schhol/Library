af77ae44aaa5dc5469bc74a50db377480f08677c
Admin <Admin@DESKTOP-T5ES982>
2019-06-12 17:09:25 +0300

    Search thingies

--- Library_System_JAVA/src/main/java/com/example/demo/model/BookRepo.java
+++ Library_System_JAVA/src/main/java/com/example/demo/model/BookRepo.java
@@ -1,13 +1,19 @@
 package com.example.demo.model;
 
-//import java.util.ArrayList;
+import java.util.ArrayList;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepo extends CrudRepository<Book, Integer> {
 	
-	//ArrayList<Book> findByOrderByTimesTakenDesc();
+	ArrayList<Book> findByOrderByTimesTakenDesc();
 
 	Book findByTitleAndAuthor(String title, String author);
+	
+	ArrayList<Book> findByTitle(String title);
+	
+	ArrayList<Book> findByAuthor(String author);
+	
+	ArrayList<Book> findByYear(int year);
 	
 }
