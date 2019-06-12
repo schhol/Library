b64ae4a7904d0dcd23e0808f446834e1560db4a4
nikin <nikin@DESKTOP-521FD5L>
2019-06-12 17:12:27 +0300

    iz gud

--- Library_System_JAVA/src/main/java/com/example/demo/model/BookRepo.java
+++ Library_System_JAVA/src/main/java/com/example/demo/model/BookRepo.java
@@ -10,14 +10,12 @@ public interface BookRepo extends CrudRepository<Book, Integer> {
 
 	Book findByTitleAndAuthor(String title, String author);
 	
-<<<<<<< HEAD
 	Book findById(int id_b);
-=======
+
 	ArrayList<Book> findByTitle(String title);
 	
 	ArrayList<Book> findByAuthor(String author);
 	
 	ArrayList<Book> findByYear(int year);
-	
->>>>>>> branch 'master' of https://github.com/schhol/Library.git
+
 }
