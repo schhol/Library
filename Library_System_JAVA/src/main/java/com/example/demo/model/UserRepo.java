c27dba8b4f925c6c950fc2618fdb4137893875f3
nikin <nikin@DESKTOP-521FD5L>
2019-06-17 16:20:02 +0300

    viskkas

--- Library_System_JAVA/src/main/java/com/example/demo/model/UserRepo.java
+++ Library_System_JAVA/src/main/java/com/example/demo/model/UserRepo.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface UserRepo extends CrudRepository<User, Integer> {
 
 	User findByUsernameAndPassword(String username, String password);
+	User findById(int id_u);
 	
 }
