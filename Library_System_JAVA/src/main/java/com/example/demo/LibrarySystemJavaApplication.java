f21c38ba07ab41cd57a8b26542726938ab14d945
nikin <nikin@DESKTOP-521FD5L>
2019-06-19 20:39:49 +0300

    colors

--- Library_System_JAVA/src/main/java/com/example/demo/LibrarySystemJavaApplication.java
+++ Library_System_JAVA/src/main/java/com/example/demo/LibrarySystemJavaApplication.java
@@ -75,7 +75,7 @@ public class LibrarySystemJavaApplication {
 			Book b2 = new Book("9438657301", 2010 ,"Biology", "Ann Lee", 4.5, "VeryGood", 4, "From single cell organisms to complicated sophisticated life forms.", ld1);
 			Book b3 = new Book("6386930284", 2011 ,"Football", "Rob Stone", 4.5, "Bad", 4, "In this book I have collected the best training methods to become great at football.", ld2);
 			Book b4 = new Book("9483958429", 2011 ,"Hockey", "Derick Johnson", 4.5, "Fine", 4, "History of hockey is amazing. Dive into the past to know why hockey is like it is now.", ld2);
-			Book b5 = new Book("9836485069", 2020 ,"Colours", "Bob Adams", 4.5, "Good", 4, "Explore the wonderful world full of colours and its meanings.", ld3);
+			Book b5 = new Book("9836485069", 2020 ,"Colors", "Bob Adams", 4.5, "Good", 4, "Explore the wonderful world full of colours and its meanings.", ld3);
 			Book b6 = new Book("2583952395", 2016 ,"Lighting", "Michael Allinson", 4.5, "VeryGood", 4, "Light is one of the most essential elements in art.", ld3);
 			
 			b1.setTimesTaken(150);
